11adb159d09b8674165ac58c3dec796756ef876b
Felipe Casado <felipecasado013@gmail.com>
2025-06-03 12:55:42 +0200

    Initial commit

---
+++ 01-Hola-Mundo/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main (String[] args){
+        String greetings = "Hola Mundo";
+        System.err.println("-------------");
+        System.out.println(greetings);
+    }
+}
+//// clsddd
