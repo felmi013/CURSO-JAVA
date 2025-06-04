52162bf8940c1b876b1636d0d9d64ec5f14aff08
Felipe Casado <felipecasado013@gmail.com>
2025-06-04 13:57:18 +0200

    Prueba dia 3

---
+++ 02-person/Greetings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Greetings {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int age;
+        System.out.println("Escribe tu nombre");
+        name = scanner.nextLine();
+        System.out.println("Escribe tu edad");
+        
+        age = scanner.nextInt();
+        System.out.println("Hola " + name);
+        System.out.println("Tienes " + age + "Años");
+       
+        scanner.close();
+        scanner.close();
+    
+
+    }
+}
