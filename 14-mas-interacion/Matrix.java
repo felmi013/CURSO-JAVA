a9dab0abadae3fe2312f45ab17e084230a04e5a9
Felipe Casado <felipecasado013@gmail.com>
2025-06-17 11:10:21 +0200

    mal

--- 14-mas-interacion/Matrix.java
+++ 14-mas-interacion/Matrix.java
@@ -1,19 +1,59 @@
 
 
 public class Matrix {
-    public static void main(String[] args) {
-        
-
+    
+    
+    static void vector(){
         int[]vectorNumber = {3,4,5,23,54};
         String[] vectorNames =new String [4];
-
+    
         vectorNames[0] = "Felipe";
         vectorNames[1] ="Luisa";  
         vectorNames[3] ="Ramon";
-
+    
         for (int i = 0; i < vectorNames.length; i++) {
             System.err.println(vectorNumber[i]);
             
         }
+        for (String name : vectorNames) {
+        System.out.println(name);
+    
+        }
+        
+    }
+
+    matrix(){
+        int[][] matrixNumber =  new int [3][3];
+
+        matrixNumber [0][0] = 22;
+        matrixNumber [0] [1] = 44;
+        matrixNumber [0] [2] = 50;
+         matrixNumber [1][0] = 25;
+        matrixNumber [1] [1] = 46;
+        matrixNumber [1] [2] = 57;
+         matrixNumber [2][0] = 13;
+        matrixNumber [2] [1] = 5;
+        matrixNumber [2] [2] = 0;
+
+
+        for (int i = 0; i < matrixNumber.length; i++) {
+            for (int j = 0; j < matrixNumber[0].length; j++) {
+                         System.out.println(matrixNumber[i][j]); 
+               //     System.out.println(matrixNumber[i][0]);
+           // System.out.println(matrixNumber[i][1]);
+           // System.out.println(matrixNumber[i][2]);
+            }
+          
+            
+        }
+    }
+
+
+
+    
+    public static void main(String[] args) {
+       vector(); 
+       matrix();
+
     }
 }
--- 14-mas-interacion/askUser.java
+++ 14-mas-interacion/askUser.java
@@ -20,32 +20,33 @@ scanner.close();
 System.out.println("Gracias por todo");
 
 }
-}
-static void ask2(){
-  Scanner scanner = new Scanner(System.in);
- String answer = "S";
 
-   }   while( !answer.toLowerCase().equals("n") &&
-            ! answer.toLowerCase().equals("no"))
+    static void ask2(){
+    Scanner scanner = new Scanner(System.in);
+    String answer = "S";
 
+        while( !answer.toLowerCase().equals("n") &&
+                ! answer.toLowerCase().equals("no")){
+
+                
+            System.out.println("¿Quieres continuar (S/N)?");
+            answer = scanner.nextLine();
+                }
             
-        System.out.println("¿Quieres continuar (S/N)?");
-        answer = scanner.nextLine();
-      
-          
+            
         
-    
-scanner.close();
-System.out.println("Gracias por todo");
+    scanner.close();
+    System.out.println("Gracias por todo");
 
 
-
+    }
 
     public static void main(String[] args) {
     //    ask();
         ask2();
     
     
+    }
 }
     
 
